cec5623d56bcd7a1b0ae18d404164fffac23dce8
michael.long <michael.long@deciphernow.com>
2018-08-06 12:32:58 -0400

    Continuing messing around with compositions

--- example/src/test/scala/monocle/JsonExample.scala
+++ example/src/test/scala/monocle/JsonExample.scala
@@ -14,87 +14,190 @@ class JsonExample extends MonocleSuite {
   case class JsArray(l: List[Json]) extends Json
   case class JsObject(m: Map[String, Json]) extends Json
 
-  val jsString = Prism[Json, String]{ case JsString(s) => Some(s); case _ => None}(JsString.apply)
-  val jsNumber = Prism[Json, Int]{ case JsNumber(n) => Some(n); case _ => None}(JsNumber.apply)
-  val jsArray  = Prism[Json, List[Json]]{ case JsArray(a) => Some(a); case _ => None}(JsArray.apply)
-  val jsObject = Prism[Json, Map[String, Json]]{ case JsObject(m) => Some(m); case _ => None}(JsObject.apply)
+  val jsString = Prism[Json, String] { case JsString(s) => Some(s); case _ => None }(JsString.apply)
+  val jsNumber = Prism[Json, Int] { case JsNumber(n) => Some(n); case _ => None }(JsNumber.apply)
+  val jsArray = Prism[Json, List[Json]] { case JsArray(a) => Some(a); case _ => None }(JsArray.apply)
+  val jsObject = Prism[Json, Map[String, Json]] { case JsObject(m) => Some(m); case _ => None }(JsObject.apply)
 
   val json: Json = JsObject(Map(
     "first_name" -> JsString("John"),
-    "last_name"  -> JsString("Doe"),
-    "age"        -> JsNumber(28),
-    "siblings"   -> JsArray(List(
+    "last_name" -> JsString("Doe"),
+    "age" -> JsNumber(28),
+    "siblings" -> JsArray(List(
       JsObject(Map(
         "first_name" -> JsString("Elia"),
-        "age"        -> JsNumber(23)
+        "age" -> JsNumber(23)
       )),
       JsObject(Map(
         "first_name" -> JsString("Robert"),
-        "age"        -> JsNumber(25)
+        "age" -> JsNumber(25)
       ))
     ))
   ))
 
   test("Json Prism") {
     jsNumber.getOption(JsString("plop")) shouldEqual None
-    jsNumber.getOption(JsNumber(2))      shouldEqual Some(2)
+    jsNumber.getOption(JsNumber(2)) shouldEqual Some(2)
+  }
+
+  test("Compose Lens with a Traversal, etc. to reach down deep and change something.") {
+    import monocle.Traversal
+    import scalaz.std.list._ // to get the Traverse instance for List
+    case class Town(residents: List[Resident])
+    case class Resident(firstName: String, lastName: String, address: Address)
+    case class Address(strNumber: Int, streetName: String, phones: List[Phone])
+    case class Phone(areaCode: Int, firstThree: Int, lastFour: Int)
+
+    val _resident = Lens[Town, List[Resident]](whole => whole.residents)(part => whole => whole.copy(residents = part))
+    val _residentT = Traversal.fromTraverse[List, Resident]
+    val _address = Lens[Resident, Address](whole => whole.address)(part => whole => whole.copy(address = part))
+    val _phones = Lens[Address, List[Phone]](whole => whole.phones)(part => whole => whole.copy(phones = part))
+    val _phonesT = Traversal.fromTraverse[List, Phone]
+
+    val trav = _resident composeTraversal _residentT composeLens _address composeLens _phones composeTraversal _phonesT
+
+    val changeAreaCodeIf540 = (phone: Phone) => phone match { //Notice that this is a total function
+      case phone if phone.areaCode == 540 => Phone(areaCode = 443, firstThree = phone.firstThree, lastFour = phone.lastFour)
+      case phone if phone.areaCode == 757 => Phone(areaCode = 804, firstThree = phone.firstThree, lastFour = phone.lastFour)
+      case _ => phone
+    }
+
+    val winchester = Town(residents = List(Resident("mike", "long", Address(103, "Leavenworth Court", phones = List(
+      Phone(333, 444, 5555),
+      Phone(757, 259, 9559),
+      Phone(540, 678, 1249),
+      Phone(540, 336, 9118),
+      Phone(540, 431, 1253)
+    )))))
+
+    val wincWithNewPhones = trav.modify(changeAreaCodeIf540)(winchester)
+    wincWithNewPhones shouldBe (Town(residents = List(Resident("mike", "long", Address(103, "Leavenworth Court", phones = List(
+      Phone(333, 444, 5555),
+      Phone(804, 259, 9559),
+      Phone(443, 678, 1249),
+      Phone(443, 336, 9118),
+      Phone(443, 431, 1253)
+    ))))))
+  }
+
+  test("Compose Lens with a Traversal, etc. to reach down deep and change something and change its type.") {
+    import monocle.Traversal
+    import scalaz.std.list._ // to get the Traverse instance for List
+    case class Town(residents: List[Resident])
+    case class Resident(firstName: String, lastName: String, address: Address)
+    case class Address(strNumber: Int, streetName: String, phones: List[Phone])
+    sealed trait Phone
+    case class PhoneInt(areaCode: Int, firstThree: Int, lastFour: Int) extends Phone
+    case class PhoneStr(phone: String) extends Phone
+
+    case class AddressStr(strNumber: Int, streetName: String, phones: List[PhoneStr])
+
+    val _resident = Lens[Town, List[Resident]](whole => whole.residents)(part => whole => whole.copy(residents = part))
+    val _residentT = Traversal.fromTraverse[List, Resident]
+    val _address = Lens[Resident, Address](whole => whole.address)(part => whole => whole.copy(address = part))
+    val _phones = Lens[Address, List[Phone]](whole => whole.phones)(part => whole => whole.copy(phones = part))
+    val _phonesT = Traversal.fromTraverse[List, Phone]
+
+    val trav = _resident composeTraversal _residentT composeLens _address composeLens _phones composeTraversal _phonesT
+
+    val changeAreaCodeIf540 = (phone: PhoneInt) => phone match { //Notice that this is a total function
+      case phone if phone.areaCode == 540 => PhoneInt(areaCode = 443, firstThree = phone.firstThree, lastFour = phone.lastFour)
+      case phone if phone.areaCode == 757 => PhoneInt(areaCode = 804, firstThree = phone.firstThree, lastFour = phone.lastFour)
+      case _ => phone
+    }
+
+    val winchester = Town(residents = List(Resident("mike", "long", Address(103, "Leavenworth Court", phones = List(
+      PhoneInt(333, 444, 5555),
+      PhoneInt(757, 259, 9559),
+      PhoneInt(540, 678, 1249),
+      PhoneInt(540, 336, 9118),
+      PhoneInt(540, 431, 1253)
+    )))))
+
+    import monocle.Iso
+    //    val personToTuple = Iso[Person, (String, Int)](p => (p.name, p.age)) { case (name, age) => Person(name, age) }
+    val phoneToStrPhone = Iso[PhoneInt, PhoneStr](p => PhoneStr(s"${p.areaCode} - ${p.firstThree} - ${p.lastFour}")) {
+      case PhoneStr(p) => PhoneInt(areaCode = p.substring(0, 3).toInt, firstThree = p.substring(4, 3).toInt, lastFour = p.substring(8, 4).toInt)
+    }
+
+
+    val trav2 = _resident composeTraversal _residentT composeLens _address composeLens _phones composeTraversal _phonesT// composeIso phoneToStrPhone
+
+
+    val changeAreaCodeIf540Str = (phone: Phone) => phone match { //Notice that this is a total function
+      case PhoneStr(phone) if phone.substring(0, 3) == "540 "=> PhoneStr(s"443-${phone.substring(3)}")
+      case PhoneStr(phone) if phone.substring(0, 3) == "757"=> PhoneStr(s"804-${phone.substring(3)}")
+      case PhoneInt(areaCode, firstThree, lastFour) if areaCode == 540 => PhoneInt(areaCode = 443, firstThree = firstThree, lastFour = lastFour)
+      case PhoneInt(areaCode, firstThree, lastFour) if areaCode == 757 => PhoneInt(areaCode = 804, firstThree = firstThree, lastFour = lastFour)
+      case _ => phone
+    }
+
+    val wincWithNewPhones = trav2.modify(changeAreaCodeIf540Str)(winchester)
+    wincWithNewPhones shouldBe (Town(residents = List(Resident("mike", "long", Address(103, "Leavenworth Court", phones = List(
+      PhoneStr("333-444-5555"),
+      PhoneStr("804-259-9559"),
+      PhoneStr("443-678-1249"),
+      PhoneStr("443-336-9118"),
+      PhoneStr("443-431-1253")
+    ))))))
+
+
   }
 
   test("Use index to go into an JsObject or JsArray") {
     (jsObject composeOptional index("age") composePrism jsNumber).getOption(json) shouldEqual Some(28)
 
     (jsObject composeOptional index("siblings")
-              composePrism    jsArray
-              composeOptional index(1)
-              composePrism    jsObject
-              composeOptional index("first_name")
-              composePrism    jsString
-    ).set("Robert Jr.")(json) shouldEqual JsObject(Map(
-      "first_name" -> JsString("John"),
-      "last_name"  -> JsString("Doe"),
-      "age"        -> JsNumber(28),
-      "siblings"   -> JsArray(List(
-        JsObject(Map(
-          "first_name" -> JsString("Elia"),
-          "age"        -> JsNumber(23)
-        )),
-        JsObject(Map(
-          "first_name" -> JsString("Robert Jr."), // name is updated
-          "age"        -> JsNumber(25)
+      composePrism jsArray
+      composeOptional index(1)
+      composePrism jsObject
+      composeOptional index("first_name")
+      composePrism jsString).set("Robert Jr.")(json) shouldEqual JsObject(Map(
+        "first_name" -> JsString("John"),
+        "last_name" -> JsString("Doe"),
+        "age" -> JsNumber(28),
+        "siblings" -> JsArray(List(
+          JsObject(Map(
+            "first_name" -> JsString("Elia"),
+            "age" -> JsNumber(23)
+          )),
+          JsObject(Map(
+            "first_name" -> JsString("Robert Jr."), // name is updated
+            "age" -> JsNumber(25)
+          ))
         ))
       ))
-    ))
   }
 
   test("Use at to add delete fields") {
     (jsObject composeLens at("nick_name")).set(Some(JsString("Jojo")))(json) shouldEqual JsObject(Map(
       "first_name" -> JsString("John"),
-      "nick_name"  -> JsString("Jojo"), // new field
-      "last_name"  -> JsString("Doe"),
-      "age"        -> JsNumber(28),
-      "siblings"   -> JsArray(List(
+      "nick_name" -> JsString("Jojo"), // new field
+      "last_name" -> JsString("Doe"),
+      "age" -> JsNumber(28),
+      "siblings" -> JsArray(List(
         JsObject(Map(
           "first_name" -> JsString("Elia"),
-          "age"        -> JsNumber(23)
+          "age" -> JsNumber(23)
         )),
         JsObject(Map(
           "first_name" -> JsString("Robert"),
-          "age"        -> JsNumber(25)
+          "age" -> JsNumber(25)
         ))
       ))
     ))
 
     (jsObject composeLens at("age")).set(None)(json) shouldEqual JsObject(Map(
       "first_name" -> JsString("John"),
-      "last_name"  -> JsString("Doe"), // John is ageless now
-      "siblings"   -> JsArray(List(
+      "last_name" -> JsString("Doe"), // John is ageless now
+      "siblings" -> JsArray(List(
         JsObject(Map(
           "first_name" -> JsString("Elia"),
-          "age"        -> JsNumber(23)
+          "age" -> JsNumber(23)
         )),
         JsObject(Map(
           "first_name" -> JsString("Robert"),
-          "age"        -> JsNumber(25)
+          "age" -> JsNumber(25)
         ))
       ))
     ))
@@ -102,46 +205,43 @@ class JsonExample extends MonocleSuite {
 
   test("Use each and filterIndex to modify several fields at a time") {
     (jsObject composeTraversal filterIndex((_: String).contains("name"))
-              composePrism     jsString
-              composeOptional  headOption
-    ).modify(_.toLower)(json) shouldEqual JsObject(Map(
-      "first_name" -> JsString("john"), // starts with lower case
-      "last_name"  -> JsString("doe"),  // starts with lower case
-      "age"        -> JsNumber(28),
-      "siblings"   -> JsArray(List(
-        JsObject(Map(
-          "first_name" -> JsString("Elia"),
-          "age"        -> JsNumber(23)
-        )),
-        JsObject(Map(
-          "first_name" -> JsString("Robert"),
-          "age"        -> JsNumber(25)
+      composePrism jsString
+      composeOptional headOption).modify(_.toLower)(json) shouldEqual JsObject(Map(
+        "first_name" -> JsString("john"), // starts with lower case
+        "last_name" -> JsString("doe"), // starts with lower case
+        "age" -> JsNumber(28),
+        "siblings" -> JsArray(List(
+          JsObject(Map(
+            "first_name" -> JsString("Elia"),
+            "age" -> JsNumber(23)
+          )),
+          JsObject(Map(
+            "first_name" -> JsString("Robert"),
+            "age" -> JsNumber(25)
+          ))
         ))
       ))
-    ))
 
-
-    (jsObject composeOptional  index("siblings")
-              composePrism     jsArray
-              composeTraversal each
-              composePrism     jsObject
-              composeOptional  index("age")
-              composePrism     jsNumber
-    ).modify(_ + 1)(json) shouldEqual JsObject(Map(
-      "first_name" -> JsString("John"),
-      "last_name"  -> JsString("Doe"),
-      "age"        -> JsNumber(28),
-      "siblings"   -> JsArray(List(
-        JsObject(Map(
-          "first_name" -> JsString("Elia"),
-          "age"        -> JsNumber(24)    // Elia is older
-        )),
-        JsObject(Map(
-          "first_name" -> JsString("Robert"),
-          "age"        -> JsNumber(26)    // Robert is older
+    (jsObject composeOptional index("siblings")
+      composePrism jsArray
+      composeTraversal each
+      composePrism jsObject
+      composeOptional index("age")
+      composePrism jsNumber).modify(_ + 1)(json) shouldEqual JsObject(Map(
+        "first_name" -> JsString("John"),
+        "last_name" -> JsString("Doe"),
+        "age" -> JsNumber(28),
+        "siblings" -> JsArray(List(
+          JsObject(Map(
+            "first_name" -> JsString("Elia"),
+            "age" -> JsNumber(24) // Elia is older
+          )),
+          JsObject(Map(
+            "first_name" -> JsString("Robert"),
+            "age" -> JsNumber(26) // Robert is older
+          ))
         ))
       ))
-    ))
   }
 
   implicit val jsonPlated: Plated[Json] = new Plated[Json] {
@@ -153,7 +253,7 @@ class JsonExample extends MonocleSuite {
     val plate: Traversal[Json, Json] = new Traversal[Json, Json] {
       def modifyF[F[_]: Applicative](f: Json => F[Json])(a: Json): F[Json] =
         a match {
-          case j@(JsString(_) | JsNumber(_)) => Applicative[F].pure(j)
+          case j @ (JsString(_) | JsNumber(_)) => Applicative[F].pure(j)
           case JsArray(l) => l.traverse(f).map(JsArray)
           case JsObject(m) => m.traverse(f).map(JsObject)
         }
@@ -168,16 +268,16 @@ class JsonExample extends MonocleSuite {
       case _ => None
     }(json) shouldEqual JsObject(Map(
       "first_name" -> JsString("JOHN"),
-      "last_name"  -> JsString("DOE"),
-      "age"        -> JsNumber(28),
-      "siblings"   -> JsArray(List(
+      "last_name" -> JsString("DOE"),
+      "age" -> JsNumber(28),
+      "siblings" -> JsArray(List(
         JsObject(Map(
           "first_name" -> JsString("ELIA"),
-          "age"        -> JsNumber(23)
+          "age" -> JsNumber(23)
         )),
         JsObject(Map(
           "first_name" -> JsString("ROBERT"),
-          "age"        -> JsNumber(25)
+          "age" -> JsNumber(25)
         ))
       ))
     ))
